de33149627c9658b5786d2ce5db96829ae3a3fe0
Edward Pierzchalski <e.a.pierzchalski@gmail.com>
2024-01-07 15:02:59 +1100

    wip

---
+++ src/infiltration_stats.ts
@@ -0,0 +1,28 @@
+import { CityName, ILocation, InfiltrationLocation, NS } from "@ns";
+
+export async function main(ns: NS): Promise<void> {
+  const locations = ns.infiltration
+    .getPossibleLocations()
+    .map<[CityName, InfiltrationLocation]>((l) => [
+      l.city,
+      ns.infiltration.getInfiltration(l.name),
+    ]);
+  locations.sort((a, b) => {
+    const a_difficulty = a[1].difficulty;
+    const b_difficulty = b[1].difficulty;
+    if (a_difficulty === b_difficulty) {
+      return a[1].reward.SoARep - b[1].reward.SoARep;
+    }
+    return a_difficulty - b_difficulty;
+  });
+  for (const [city, inf] of locations) {
+    ns.tprintf(
+      "%s: %s: difficulty(%s), reward(SoA: %s, Other: %s)",
+      inf.location.city,
+      inf.location.name,
+      inf.difficulty,
+      inf.reward.SoARep,
+      inf.reward.tradeRep
+    );
+  }
+}
